0a049713bdcb6668da71d975dae09217c555fc4a
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-02-09 23:50:51 +0530

    Update index.js

--- SearchList/index.js
+++ SearchList/index.js
@@ -1,15 +1,27 @@
 import './index.css'
 
 const SearchDetailsList = props => {
-  const {id, timeAccessed, logoUrl, title, domainUrl} = props
+  const {timeAccessed, logoUrl, title, domainUrl} = props
   return (
     <li className="list-details-container">
       <p className="time">{timeAccessed}</p>
       <div className="inner-list-container">
         <div className="except-delete-details">
-          <img src={logoUrl} alt="domain logo" className="logo-image" />
-          <p className="title">{title}</p>
-          <p className="url">{domainUrl}</p>
+          <div className="only-logo">
+            <img src={logoUrl} alt="domain logo" className="logo-image" />
+          </div>
+          <div className="title-url">
+            <p className="title">{title}</p>
+            <p className="url">{domainUrl}</p>
+          </div>
+        </div>
+        <div className="delete-image-container">
+          <button type="button" data-testid="delete" className="delete-image">
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+              alt="delete"
+            />
+          </button>
         </div>
       </div>
     </li>
